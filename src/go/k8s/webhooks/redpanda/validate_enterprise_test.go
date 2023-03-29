44b05e87584c6f7d1ec0a07b491dc237c690828d
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-29 01:31:17 -0400

    spelling: test
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/go/k8s/webhooks/redpanda/validate_enterprise_test.go
+++ src/go/k8s/webhooks/redpanda/validate_enterprise_test.go
@@ -52,7 +52,7 @@ func TestDoNotValidateWhenDeleted(t *testing.T) {
 	}
 
 	cfg, err := testEnv.Start()
-	defer testEnv.Stop() //nolint:errcheck // in test test env error is not relevant
+	defer testEnv.Stop() //nolint:errcheck // in test env error is not relevant
 	require.NoError(t, err)
 	require.NotNil(t, cfg)
 
