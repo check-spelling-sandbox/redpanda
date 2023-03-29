aef8e23457009857cdef2bbc5113f59557da6ca9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-29 03:26:48 -0400

    spelling: creates
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/rptest/tests/controller_erase_test.py
+++ tests/rptest/tests/controller_erase_test.py
@@ -64,7 +64,7 @@ class ControllerEraseTest(RedpandaTest):
             segments = storage.ns['redpanda'].topics['controller'].partitions[
                 "0_0"].segments.keys()
             # We expect that segments count for controller should be transfers_leadership_count + 1.
-            # Because each transfer creats one segment + initial leadership after restart creates first segment
+            # Because each transfer creates one segment + initial leadership after restart creates first segment
             return len(segments) == transfers_leadership_count + 1
 
         wait_until(
