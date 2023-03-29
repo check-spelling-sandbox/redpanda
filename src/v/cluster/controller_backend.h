72b3cd29b2b1cc9f9c9ebda453f6dfc5f5bb44f2
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-29 03:26:29 -0400

    spelling: contain
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/v/cluster/controller_backend.h
+++ src/v/cluster/controller_backend.h
@@ -279,7 +279,7 @@ private:
      * operation was evaluating but hasn't finished yet.
      * We assume that we can leave orphan files only on redpanda restart
      * so we run clean process on startup, after bootstrap when redpanda
-     * already knows about all topics that it should containt on disk
+     * already knows about all topics that it should contain on disk
      **/
     ss::future<> clear_orphan_topic_files(
       model::revision_id bootstrap_revision,
