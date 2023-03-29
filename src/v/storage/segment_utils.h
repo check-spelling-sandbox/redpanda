582440dc3fe7bbf8409690a3877ed852e41dd565
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-29 03:26:15 -0400

    spelling: concatenate
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/v/storage/segment_utils.h
+++ src/v/storage/segment_utils.h
@@ -41,7 +41,7 @@ ss::future<compaction_result> self_compact_segment(
   offset_delta_time apply_offset);
 
 /*
- * Concatentate segments into a minimal new segment.
+ * Concatenate segments into a minimal new segment.
  *
  * This is effectively equivalent to acquiring the proper locks while
  * concatenating segment data into the given path and then building an open
