e8797454e041fe2bd21af99f304589700db959fd
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-29 21:50:52 -0400

    spelling: truncated
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/v/archival/archival_policy.cc
+++ src/v/archival/archival_policy.cc
@@ -246,7 +246,7 @@ static ss::future<std::optional<std::error_code>> get_file_range(
     upl->max_timestamp = segment->index().max_timestamp();
     if (!end_inclusive && segment->offsets().base_offset == begin_inclusive) {
         // Fast path, the upload is started at the beginning of the segment
-        // and not truncted at the end.
+        // and not truncated at the end.
         vlog(
           archival_log.debug,
           "Full segment upload {}, file size: {}",
