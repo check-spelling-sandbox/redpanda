2690d558c107bbdc9a104d3f58e27484bcd43355
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-29 21:49:34 -0400

    spelling: returned
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/v/raft/state_machine.h
+++ src/v/raft/state_machine.h
@@ -100,7 +100,7 @@ public:
      * Sends a round of heartbeats to followers, when majority of followers
      * replied with success to either this of any following request all reads up
      * to returned offsets are linearizable. (i.e. majority of followers have
-     * updated their commit indices to at least reaturned offset). For more
+     * updated their commit indices to at least returned offset). For more
      * details see paragraph 6.4 of Raft protocol dissertation.
      */
     ss::future<result<model::offset>>
