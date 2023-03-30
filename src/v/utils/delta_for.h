f7c3d024dff0b5a8e2ba4812e16f6676af44e69c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-29 21:50:14 -0400

    spelling: subtracting
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/v/utils/delta_for.h
+++ src/v/utils/delta_for.h
@@ -65,7 +65,7 @@ struct delta_xor {
  *
  * The encoder can work only with non-decreasing integer sequences
  * with 64-bit values. It uses delta-delta algorithm to compute delta values.
- * The alg. computes delta values by subtacting consecutive values from
+ * The alg. computes delta values by subtracting consecutive values from
  * each other and then it subtracts the pre-defined step value out of every
  * delta. The step value is a minimal possible delta between two consecutive
  * elements.
