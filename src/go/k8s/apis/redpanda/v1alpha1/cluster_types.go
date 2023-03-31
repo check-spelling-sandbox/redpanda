6c176ac6ac632cfdc62d954ab5233f51f5f4812a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-31 02:31:09 -0400

    spelling: truststore
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/go/k8s/apis/redpanda/v1alpha1/cluster_types.go
+++ src/go/k8s/apis/redpanda/v1alpha1/cluster_types.go
@@ -653,7 +653,7 @@ type LoadBalancerStatus struct {
 // If Enabled is set to true, one-way TLS verification is enabled.
 // In that case, a key pair ('tls.crt', 'tls.key') and CA certificate 'ca.crt'
 // are generated and stored in a Secret with the same name and namespace as the
-// Redpanda cluster. 'ca.crt', must be used by a client as a trustore when
+// Redpanda cluster. 'ca.crt', must be used by a client as a truststore when
 // communicating with Redpanda.
 //
 // If RequireClientAuth is set to true, two-way TLS verification is enabled.
